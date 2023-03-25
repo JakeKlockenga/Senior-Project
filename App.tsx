bccf3047928b71ec677e98a9ed5b6a8ddf4cc663
Sok <92661051+Sok5@users.noreply.github.com>
2023-03-24 21:39:39 -0500

    Add document scanner to scan and crop image automatically by detecting the corners of the image

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, Button, Image, StatusBar } from 'react-native';
-import {launchCamera, launchImageLibrary} from "react-native-image-picker";
-import ActionSheet1 from 'react-native-ui-action-sheet';
-import ActionSheet2 from 'react-native-ui-action-sheet';
-
+import DocumentScanner from 'react-native-document-scanner-plugin';
 
 async function CallGoogleCloudVisionAPI(image) {
 	const body = {
@@ -21,7 +18,7 @@ async function CallGoogleCloudVisionAPI(image) {
 			}
 		]
 	};
-	const response = await fetch(`https://us-vision.googleapis.com/v1/images:annotate?key=`, {
+	const response = await fetch(`https://us-vision.googleapis.com/v1/images:annotate?key=${"API_key"}`, {
 		method: 'POST',
 		headers: {
 			Accept: 'application/json',
@@ -31,32 +28,7 @@ async function CallGoogleCloudVisionAPI(image) {
 	});
 	const result = await response.json();
 	const recognizedText = result.responses[0].fullTextAnnotation;
-	//console.log("recognizedText.text: ", recognizedText.text);
-	const textArray = [];
-	recognizedText.pages[0].blocks.forEach(block => {
-		block.paragraphs.forEach(paragraph => {
-			paragraph.words.forEach(word => {
-				let wordArray = [];
-				// this is the bounding box for the word
-				//console.log("Bounding box vertices for word: ", word.boundingBox.vertices);
-				// word is not listed in this object as whole, but instead as separate "symbols" which are letters in most cases
-				word.symbols.forEach(letter => { //adding each letter to an array so that we can reconstruct them into a word
-					wordArray.push(letter.text);
-				})
-				//regex to remove commas separating letters in array after converting it to a string thus leaving only the word
-				let finalWord = wordArray.toString().replace(/\,/g, '');
-				if (finalWord.length != 0) {
-					let wordObject =
-						{
-							vertices: word.boundingBox.vertices,
-							word: finalWord
-						}
-					textArray.push(wordObject);
-				}
-			})
-		})
-	})
-	return textArray ? textArray : {text: "no text recognized on this image."};
+	return recognizedText ? recognizedText : {text: "no text recognized on this image."};
 }
 
 export default function App() {
@@ -65,14 +37,46 @@ export default function App() {
 	const [image1, setImage1] = useState(null);
 	const [image2, setImage2] = useState(null);
 	const [compareText, setCompareText] = useState("Need two images to compare");
-	const ActionSheetRef1 = useRef();
-	const ActionSheetRef2 = useRef();
-	const noise = [];
-	const matches = [];
+	const [buttonImage, setButtonImage] = useState();
+	const [scannedImage, setScannedImage] = useState();
+	const scanDocument = async () => {
+		const { scannedImages } = await DocumentScanner.scanDocument({
+			croppedImageQuality: 100,
+			maxNumDocuments: 1,
+			responseType: 'base64'
+		})
+		
+		if (scannedImages.length > 0) {
+			setScannedImage(scannedImages[0])
+		}
+	}
+
+    useEffect(()=>{
+        (async()=>{
+			if(scannedImage) {
+				try {
+					if (buttonImage == "buttonImage1") {
+						setImage1(scannedImage);
+						setText1("loading...");
+						const response = await CallGoogleCloudVisionAPI(scannedImage);
+						setText1(response.text);
+					}
+					else if (buttonImage == "buttonImage2") {
+						setImage2(scannedImage);
+						setText2("loading...");
+						const response = await CallGoogleCloudVisionAPI(scannedImage);
+						setText2(response.text);
+					}
+				} catch (e) {
+					console.log('e', e);
+				}
+			}
+		})();
+	},[scannedImage]);
 
 	useEffect(()=>{
-		if (text1 != null && text2 != null && text1 != "loading..." && text2 != "loading...") {
-/*			if (text1 == "no text recognized on this image." && text2 == "no text recognized on this image.")
+		if (text1 != null && text2 != null) {
+			if (text1 == "no text recognized on this image." && text2 == "no text recognized on this image.")
 			{
 				setCompareText("No text recognized on these images!");
 			}
@@ -83,59 +87,8 @@ export default function App() {
 			else
 			{
 				setCompareText("Image text does NOT match");
-			}*/
-			//const splitText1 = text1.split(/\s/);
-			//const splitText2 = text2.split(/\s/);
-			//const matches = [];
-/*			splitText1.forEach(word =>
-			{
-				let matched = false;
-				if (word.includes(",") || word.includes("."))
-				{
-					word = word.substring(0,word.length-1);
-				}
-				splitText2.forEach(word2 =>
-				{
-					if (word2.includes(",") || word2.includes("."))
-					{
-						word2 = word2.substring(0,word2.length-1);
-					}
-
-					if (word == word2)
-					{
-						matched = true;
-						matches.push(word);
-					}
-				})
-				if (!matched)
-				{
-					noise.push(word);
-				}
-			})
-			console.log("matches: ",matches.flat());
-			console.log("noise: ",noise.flat());*/
-			text2.forEach(object2 =>
-			{
-				if (!object2.word.includes(" ") && !object2.word.includes(".") && !object2.word.includes(",") && !object2.word.includes('\n'))
-				{
-					text1.forEach(object1 =>
-					{
-						if (object1.word == object2.word)
-						{
-							matches.push(object1);
-						}
-						else
-						{
-							noise.push(object1);
-						}
-					})
-				}
-			})
+			}
 		}
-		matches.forEach(match =>
-		{
-			console.log(match.word);
-		})
 	},[text1,text2]);
 
 	return (
@@ -144,70 +97,27 @@ export default function App() {
 				<Text style={styles.title}>ColdSpring Plate Checking</Text>
 			</View>
 			<Text style={{fontSize: 3,}}>{' '}</Text>
-			{image1?<Image source={{uri: image1}} style={{flex: 1, width: '100%', height: '100%', resizeMode: 'contain',}}/>:null}
-			{/*<View style={{width: 391}}>
+			{image1?<Image source={{uri: "data:image/jpg;base64,"+image1}} style={{flex: 1, width: '100%', height: '100%', resizeMode: 'contain',}}/>:null}
+			<View style={{width: 391}}>
 				{text1?<Text>Recognized text is {text1}</Text>:null}
-			</View>*/}
-			<View style={{padding: 5,}}>
-				<Button title="Add Set Plate" onPress={()=>ActionSheetRef1.current.show()}/>
 			</View>
-			<ActionSheet1
-				ref={ActionSheetRef1}
-				title={'Select a Picture'}
-				options={[
-					{title: 'Take Picture 1', onPress: async () => {
-						let result = await launchCamera({
-							includeBase64: true
-						});
-						setImage1(result.assets[0].uri);
-						const response = await CallGoogleCloudVisionAPI(result.assets[0].base64);
-						setText1(response);
-					}},
-					{title: 'Choose Picture 1 from image library', onPress: async () => {
-						let result = await launchImageLibrary({
-							includeBase64: true
-						});
-						setImage1(result.assets[0].uri);
-						setText1("loading...");
-						const response = await CallGoogleCloudVisionAPI(result.assets[0].base64);
-						setText1(response);
-					}},
-				]}
-				cancelTitle="cancel"
-			/>
-			{image2?<Image source={{uri:image2}} style={{flex: 1, width: '100%', height: '100%', resizeMode: 'contain',}}/>:null}
-			{/*<View style={{width: 391}}>
+			<View style={{padding: 5,}}>
+				<Button title="Add Picture 1" onPress={()=> {
+				scanDocument();
+				setButtonImage("buttonImage1");
+				}}/>
+			</View>
+			{image2?<Image source={{uri: "data:image/jpg;base64,"+image2}} style={{flex: 1, width: '100%', height: '100%', resizeMode: 'contain',}}/>:null}
+			<View style={{width: 391}}>
 				{text2?<Text>Recognized text is {text2}</Text>:null}
-			</View>*/}
-			<View style={{padding: 5,}}>
-				<Button title="Add Design Template" onPress={()=>ActionSheetRef2.current.show()}/>
 			</View>
-			<ActionSheet2
-				ref={ActionSheetRef2}
-				title={'Select a Picture'}
-				options={[
-					{title: 'Take Picture 2', onPress: async () => {
-						let result = await launchCamera({
-							includeBase64: true
-						});
-						setImage2(result.assets[0].uri);
-						const response = await CallGoogleCloudVisionAPI(result.assets[0].base64);
-						setText2(response);
-					}},
-					{title: 'Choose Picture 2 from image library', onPress: async () => {
-						let result = await launchImageLibrary({
-							includeBase64: true
-						});
-						setImage2(result.assets[0].uri);
-						setText2("loading...");
-						const response = await CallGoogleCloudVisionAPI(result.assets[0].base64);
-						setText2(response);
-					}},
-				]}
-				cancelTitle="cancel"
-			/>
+			<View style={{padding: 5,}}>
+				<Button title="Add Picture 2" onPress={()=> {
+				scanDocument();
+				setButtonImage("buttonImage2");
+				}}/>
+			</View>
 			<Text style={{color: text1 && text2 && text1.toString() == text2.toString() ? 'green':'red', fontSize:30, textAlign:'center'}}>{compareText}</Text>
-			{noise ? noise.map(word=><Text>word</Text>) : null}
 			<StatusBar style="auto"/>
 		</View>
 	);
@@ -232,5 +142,5 @@ const styles = StyleSheet.create({
 		fontSize: 17,
 		textAlign: 'center',
 		fontWeight: 'bold',
-	}
+	},
 });
