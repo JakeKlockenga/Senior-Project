9b48a4c3bed39c5a9791d43b479bbac700414b0e
Sok <92661051+Sok5@users.noreply.github.com>
2023-04-24 20:08:36 -0500

    Redesign UI to add three tabs and display highlighted differences between the recognized text of the template and the recognized text of the plate

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, Button, Image, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, Image, StatusBar, ScrollView, Pressable } from 'react-native';
 import DocumentScanner from 'react-native-document-scanner-plugin';
 import PhotoEditor from '@baronha/react-native-photo-editor';
 import ImgToBase64 from 'react-native-image-base64';
+import { TabView, Tab } from 'react-native-tab-view-easy';
+import { TabBar } from 'react-native-tab-view';
+import Diff from "react-native-diff-component";
 
 async function CallGoogleCloudVisionAPI(image) {
 	const body = {
@@ -13,14 +16,14 @@ async function CallGoogleCloudVisionAPI(image) {
 				},
 				"features": [
 					{
-						"type": "TEXT_DETECTION", 
+						"type": "TEXT_DETECTION",
 						"maxResults": 1
 					}
 				]
 			}
 		]
 	};
-	const response = await fetch(`https://us-vision.googleapis.com/v1/images:annotate?key=${"API_key"}`, {
+	const response = await fetch(`https://us-vision.googleapis.com/v1/images:annotate?key=API_key`, {
 		method: 'POST',
 		headers: {
 			Accept: 'application/json',
@@ -34,7 +37,7 @@ async function CallGoogleCloudVisionAPI(image) {
 }
 
 function FilteredText(detectedText) {
-	const filteredText = detectedText.replace(/[^A-Za-z0-9`~!@#$%^&*()-_=+\[{\]}\|;:'",<.>/?\s]+\s|[^A-Za-z0-9`~!@#$%^&*()-_=+\[{\]}\|;:'",<.>/?\s]|937+\s|937|\++\s|\+|\b1\b|(#.*)+\s|(GB-.*)+\s|(.*x.*)+\s|(.*X.*)+\s/g, '');
+	const filteredText = detectedText.replace(/[^A-Za-z0-9`~!@#$%^&*()-_=+\[{\]}\|;:'",<.>/?\s]\s|^[^A-Za-z0-9`~!@#$%^&*()-_=+\[{\]}\|;:'",<.>/?\s]$|\b937\s|\b937\b|\+\s|\+|\b1\s|\b1\b|(#[^A-Za-z]*)\s|(GB[^A-Za-z]*)\s|([^A-Za-z]*x[^A-Za-z]*)\s|([^A-Za-z]*X[^A-Za-z]*)\s|\bt\s|\bt\b|\bT\s|\bT\b|\bO\s|\bO\b|\bo\s|\bo\b|(NS[^A-Za-z]*)\s|\$\s|\$|(CF[^A-Za-z]*)\s|\*\s|\*/g, '');
 	return filteredText;
 }
 
@@ -47,21 +50,33 @@ export default function App() {
 	const [buttonImage, setButtonImage] = useState();
 	const [scannedImage, setScannedImage] = useState();
 	const [imageBase64String, setImageBase64String] = useState();
-	
+	const [difference, setDifference] = useState();
+
 	const scanDocument = async () => {
 		const { scannedImages } = await DocumentScanner.scanDocument({
 			croppedImageQuality: 100,
 			maxNumDocuments: 1,
 			responseType: 'imageFilePath'
 		})
-		
+
 		if (scannedImages.length > 0) {
 			setScannedImage(scannedImages[0])
 		}
-	}
+	};
 
-    useEffect(()=>{
-        (async()=>{
+	const renderTabBar = props => (
+		<TabBar
+			{...props}
+			indicatorStyle={{ backgroundColor: 'dodgerblue' }}
+			style={{ backgroundColor: 'white', marginBottom: 5 }}
+			labelStyle={{ textTransform: 'capitalize', fontSize: 15, fontWeight: 'bold' }}
+			activeColor='black'
+			inactiveColor='black'
+		/>
+	);
+
+	useEffect(()=>{
+		(async()=>{
 			if(scannedImage) {
 				try {
 					const path = await PhotoEditor.open({
@@ -109,14 +124,17 @@ export default function App() {
 			if (text1 == "no text recognized on this image." && text2 == "no text recognized on this image.")
 			{
 				setCompareText("No text recognized on these images!");
+				setDifference(false);
 			}
 			else if (text1.toString() == text2.toString())
 			{
 				setCompareText("Image text matches!");
+				setDifference(false);
 			}
 			else
 			{
-				setCompareText("Image text does NOT match");
+				setCompareText("Image text does NOT match.");
+				setDifference(true);
 			}
 		}
 	},[text1,text2]);
@@ -124,31 +142,84 @@ export default function App() {
 	return (
 		<View style={styles.container}>
 			<View style={styles.header}>
-				<Text style={styles.title}>ColdSpring Plate Checking</Text>
+				<Text style={styles.title}>Coldspring Plate Checking</Text>
 			</View>
-			<Text style={{fontSize: 3,}}>{' '}</Text>
-			{image1?<Image source={{uri: image1}} style={{flex: 1, width: '100%', height: '100%', resizeMode: 'contain',}}/>:null}
-			<View style={{left: 5, width: 386}}>
-				{text1?<Text>Recognized text is {text1}</Text>:null}
-			</View>
-			<View style={{padding: 5,}}>
-				<Button title="Add Picture 1" onPress={()=> {
-				scanDocument();
-				setButtonImage("buttonImage1");
-				}}/>
-			</View>
-			{image2?<Image source={{uri:image2}} style={{flex: 1, width: '100%', height: '100%', resizeMode: 'contain',}}/>:null}
-			<View style={{left: 5, width: 386}}>
-				{text2?<Text>Recognized text is {text2}</Text>:null}
-			</View>
-			<View style={{padding: 5,}}>
-				<Button title="Add Picture 2" onPress={()=> {
-				scanDocument();
-				setButtonImage("buttonImage2");
-				}}/>
-			</View>
-			<Text style={{color: text1 && text2 && text1.toString() == text2.toString() ? 'green':'red', fontSize:30, textAlign:'center'}}>{compareText}</Text>
-			<StatusBar style="auto"/>
+			<TabView renderTabBar={renderTabBar}>
+				<Tab title={'Template'}>
+					<View style={styles.image}>
+						{image1?<Image source={{uri: image1}} style={{flex: 1, width: '100%', height: '100%', resizeMode: 'contain',}}/>:null}
+					</View>
+					<View style={styles.recognizedText}>
+						{text1?
+							<View style={{paddingLeft: 5, paddingRight: 5, paddingTop: 10}}>
+								<Text style={{fontWeight: "bold"}}>Recognized text:{"\n"}</Text>
+								<ScrollView>
+									<Text>{text1}</Text>
+								</ScrollView>
+							</View>
+						:null}
+					</View>
+					<Pressable
+						onPress={() => {
+						scanDocument();
+						setButtonImage("buttonImage1");
+					}}>
+						<Text style={styles.button}>Add Template</Text>
+					</Pressable>
+				</Tab>
+
+				<Tab title={'Plate'}>
+					<View style={styles.image}>
+						{image2?<Image source={{uri: image2}} style={{flex: 1, width: '100%', height: '100%', resizeMode: 'contain',}}/>:null}
+					</View>
+					<View style={styles.recognizedText}>
+						{text2?
+							<View style={{paddingLeft: 5, paddingRight: 5, paddingTop: 10}}>
+								<Text style={{fontWeight: "bold"}}>Recognized text:{"\n"}</Text>
+								<ScrollView>
+									<Text>{text2}</Text>
+								</ScrollView>
+							</View>
+						:null}
+					</View>
+					<Pressable
+						onPress={() => {
+						scanDocument();
+						setButtonImage("buttonImage2");
+					}}>
+						<Text style={styles.button}>Add Plate</Text>
+					</Pressable>
+				</Tab>
+
+				<Tab title={'Result'}>
+					<Text style={{color: text1 && text2 && text1.toString() == text2.toString() ? 'green':'red', fontSize:30, textAlign:'center'}}>{compareText}</Text>
+					{difference?
+						<View style={styles.differentTexts}>		
+							<View style={styles.plateDifferentText}>
+								{text1 && text2?
+									<View style={{paddingLeft: 5, paddingRight: 5, paddingTop: 10}}>
+										<Text style={{fontWeight: "bold"}}>Plate is different from Template:{"\n"}</Text>
+										<ScrollView>
+											<Diff inputA={text1.replace(/\n/g, ' ')} inputB={text2.replace(/\n/g, ' ')} type="words" />
+										</ScrollView>
+									</View>
+								:null}
+							</View>
+							<View style={styles.templateDifferentText}>
+								{text2 && text1?
+									<View style={{paddingLeft: 5, paddingRight: 5, paddingTop: 10}}>
+										<Text style={{fontWeight: "bold"}}>Template is different from Plate:{"\n"}</Text>
+										<ScrollView>
+											<Diff inputA={text2.replace(/\n/g, ' ')} inputB={text1.replace(/\n/g, ' ')} type="words" />
+										</ScrollView>
+									</View>
+								:null}
+							</View>
+						</View>
+					:false}
+					<StatusBar style="auto"/>
+				</Tab>
+			</TabView>
 		</View>
 	);
 }
@@ -156,10 +227,7 @@ export default function App() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-		flexDirection: 'column',
+		backgroundColor: 'white',
 	},
 	header: {
 		backgroundColor: 'royalblue',
@@ -172,5 +240,37 @@ const styles = StyleSheet.create({
 		fontSize: 17,
 		textAlign: 'center',
 		fontWeight: 'bold',
+	},
+	image: {
+		flex: 1,
+	},
+	recognizedText: {
+		flex: 1.3,
+		paddingBottom: 78,
+	},
+	button: {
+		color: 'black',
+		fontWeight: 'bold',
+		fontSize: 15,
+		borderRadius: 6,
+		backgroundColor : 'lightskyblue',
+		textAlign: 'center',
+		paddingLeft : 12,
+		paddingRight : 12,
+		paddingBottom : 6,
+		paddingTop: 6,
+		alignSelf: 'center',
+		position: 'absolute',
+		bottom: 5,
+	},
+	differentTexts: {
+		flex: 1,
+	},
+	plateDifferentText: {
+		flex: 1,
+		paddingBottom: 45,
+	},
+	templateDifferentText: {
+		flex: 1.2,
 	},
 });
